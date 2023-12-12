4fcadba3a4609c3a5eaa22c02633c33fe8871b98
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2023-12-12 15:53:14 -0300

    act 12-12 a

--- contacto/forms.py
+++ contacto/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre=forms.CharField(label="Nombre", required=True)
     email=forms.CharField(label="Email", required=True)
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido", widget=forms.Textarea )
 
